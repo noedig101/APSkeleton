d647737812041f25dafe50d4756cc91828f6ec97
Hunter Oppel <hun.oppel@gmail.com>
2025-05-18 15:55:16 -0400

    More comments

--- Rules.py
+++ Rules.py
@@ -6,6 +6,11 @@ if TYPE_CHECKING:
 
 # This is the last big thing to do (at least for me)
 # This is where you add item
+# These are omega simplified rules
+# There are a ton of different ways you can add rules from amoount of items you need to optional items
+# Theres also difficulty options and a bunch others
+# Id suggest going through a bunch of different ap worlds and seeing how they do the rules
+# Even better if its a game you know a lot about and can tell what you need to get to certain locations
 def set_rules(world: "APSkeletonWorld"):
     player = world.player
     options = world.options
@@ -21,4 +26,5 @@ def set_rules(world: "APSkeletonWorld"):
     add_rule(world.multiworld.get_entrance("The Sewer -> Big Hole in the Floor", player),
              lambda state: state.has("A cute rat") and state.has("Estrogen") and state.has("Testosterone"))
     
+    # Victory condition rule!
     world.multiworld.completion_condition[player] = lambda state: state.has("Victory", player)
